f081c40321acf22789fe4112744b61540afaa0dc
GhizlaneMeq <meqdarghizlane@gmail.com>
2024-12-19 09:54:13 +0100

    update files "

--- src/test/java/ma/hmzelidrissi/bankmanagementsystem/repositories/AbstractTestcontainers.java
+++ src/test/java/ma/hmzelidrissi/bankmanagementsystem/repositories/AbstractTestcontainers.java
@@ -8,21 +8,5 @@ import org.testcontainers.containers.PostgreSQLContainer;
  * Abstract class for testcontainers.
  */
 public abstract class AbstractTestcontainers {
-    protected static final PostgreSQLContainer<?> POSTGRES;
 
-    static {
-        POSTGRES = new PostgreSQLContainer<>("postgres:17")
-                .withDatabaseName("test_db")
-                .withUsername("test")
-                .withPassword("test");
-
-        POSTGRES.start();
-    }
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
-        registry.add("spring.datasource.username", POSTGRES::getUsername);
-        registry.add("spring.datasource.password", POSTGRES::getPassword);
-    }
 }
